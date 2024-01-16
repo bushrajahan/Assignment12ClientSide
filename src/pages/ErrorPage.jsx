f616b17d2aea222d31ef9e0d3e1361ba267faf38
bushrajahan <132329761+bushrajahan@users.noreply.github.com>
2024-01-16 22:02:04 +0600

    FInally Finished

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ErrorPage = () => {
+  const navigate = useNavigate()
+  const backgroundImageStyle = {
+    backgroundImage: 'url("https://i.ibb.co/x8tCNX9/7741849-3747371.jpg")',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh', // Set the height of the div to fill the viewport
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
+   const hanldeClick = () =>{
+      navigate('/')
+   }
   return (
-    <div>
-      
+    <div style={backgroundImageStyle}>
+      {/* Content of the error page */}
+      <button className='btn ' onClick={hanldeClick}>Go To home</button>
     </div>
   );
 };
